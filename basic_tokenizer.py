a0b2d59fba2229ba220a2b3c613469dd37d749a6
DanTheMan314 <39549968+DanTheMan314@users.noreply.github.com>
2025-01-25 23:59:07 +0530

    third things third

---
+++ basic_tokenizer.py
@@ -0,0 +1,5 @@
+import string
+tokenized = []
+
+file = open("tokenized.txt","w")
+file.write(tokenized)
---
+++ text_cleaner.py
@@ -0,0 +1,7 @@
+import re
+import string
+cleaningchars = [".",",","‘","’","!","?",":","—","-","\"","'",";","*","*","","(",")","…","/","–","•","@","=","|","“","\u200d","+","\u200c"]
+cleaned_text = ""
+
+file = open("cleaned_text.txt","w")
+file.write(cleaned_text)
